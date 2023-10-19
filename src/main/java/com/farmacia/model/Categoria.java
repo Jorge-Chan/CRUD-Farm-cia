f2564d3f509ce4a862e4da419dcd34cac4cfcafc
Avontss <jorgemchan@gmail.com>
2023-10-19 20:28:54 -0300

    Implementação do Crud de produto

--- src/main/java/com/farmacia/model/Categoria.java
+++ src/main/java/com/farmacia/model/Categoria.java
@@ -1,11 +1,16 @@
 package com.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -26,6 +31,17 @@ public class Categoria {
 	@Size(min = 3, max = 1000)
 	private String descricao;
 
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 	public Long getId() {
 		return id;
 	}
